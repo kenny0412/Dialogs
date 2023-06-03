afbafd454e0461e209c2fe1149348c82895e10ff
Kenny Cardenas <31745798+kenny0412@users.noreply.github.com>
2023-06-03 15:15:53 -0600

    Dialogs -> Enable viewBinding

--- app/src/main/java/com/example/dialogs/MainActivity.kt
+++ app/src/main/java/com/example/dialogs/MainActivity.kt
@@ -2,15 +2,19 @@ package com.example.dialogs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dialogs.databinding.ActivityMainBinding
 import com.example.dialogs.nativedialog.NativeDialog
 
 class MainActivity : AppCompatActivity() {
     private val nativeDialog: NativeDialog by lazy {
         NativeDialog()
     }
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         nativeDialog.nativeAlertDialog(this@MainActivity)
     }
